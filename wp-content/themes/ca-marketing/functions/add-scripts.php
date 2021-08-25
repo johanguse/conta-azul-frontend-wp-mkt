43106cf7b89032c2bee988171cb81bb216188aef
Johan Guse <johanguse@gmail.com>
2021-08-25 16:22:54 -0300

    fix ajax enqueue

--- wp-content/themes/ca-marketing/functions/add-scripts.php
+++ wp-content/themes/ca-marketing/functions/add-scripts.php
@@ -12,19 +12,15 @@ function theme_scripts() {
 	if ( ! is_admin() ) {
 		wp_deregister_script( 'jquery' ); // Unregister WordPress built-in jquery.
 
-		wp_register_script( 'jquery', 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js', false, '3.5.1', true );
+		wp_register_script('jquery-3.3.1', 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js', false, '3.3.1', true);
+		wp_enqueue_script('jquery-3.3.1');
 
 		//wp_enqueue_script( 'vendorjs', get_template_directory_uri() . '/assets/js/vendor.min.js', array( 'jquery' ), filemtime( get_template_directory() . '/assets/js/vendor.min.js' ), true );
-		wp_enqueue_script( 'customjs', get_template_directory_uri() . '/assets/js/main.min.js', array( 'jquery' ), filemtime( get_template_directory() . '/assets/js/main.min.js' ), true );
-		wp_enqueue_script( array( 'jquery', 'customjs' ) );
+		wp_enqueue_script( 'customjs', get_template_directory_uri() . '/assets/js/main.min.js');
 
-		wp_localize_script(
-			'jquery',
-			'ajax_url',
-			array(
-				'ajaxurl' => admin_url( 'admin-ajax.php' ),
-			)
-		);
+		wp_localize_script( 'jquery-3.3.1', 'ajax_url', array(
+			'ajaxurl'       => admin_url( 'admin-ajax.php' )
+		));
 
 	}
 }
