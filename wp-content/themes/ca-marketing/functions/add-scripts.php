f307e92372da43743a068ed965db5ec9683bedf8
Johan Guse <johanguse@gmail.com>
2021-08-25 13:34:57 -0300

    fix register styles and js

--- wp-content/themes/ca-marketing/functions/add-scripts.php
+++ wp-content/themes/ca-marketing/functions/add-scripts.php
@@ -13,11 +13,10 @@ function theme_scripts() {
 		wp_deregister_script( 'jquery' ); // Unregister WordPress built-in jquery.
 
 		wp_register_script( 'jquery', 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js', false, '3.5.1', true );
-		wp_register_script( 'bootstrap', 'https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/js/bootstrap.min.js', false, '4.6.0', true );
 
 		//wp_enqueue_script( 'vendorjs', get_template_directory_uri() . '/assets/js/vendor.min.js', array( 'jquery' ), filemtime( get_template_directory() . '/assets/js/vendor.min.js' ), true );
 		wp_enqueue_script( 'customjs', get_template_directory_uri() . '/assets/js/main.min.js', array( 'jquery' ), filemtime( get_template_directory() . '/assets/js/main.min.js' ), true );
-		wp_enqueue_script( array( 'jquery', 'bootstrap', 'vendorjs', 'customjs' ) );
+		wp_enqueue_script( array( 'jquery', 'customjs' ) );
 
 		wp_localize_script(
 			'jquery',
--- wp-content/themes/ca-marketing/functions/add-styles.php
+++ wp-content/themes/ca-marketing/functions/add-styles.php
@@ -10,10 +10,9 @@
  */
 function theme_styles() {
 	if ( ! is_admin() ) {
-		wp_register_style( 'bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css', false, '4.6.0', false );
 		wp_register_style( 'style', get_template_directory_uri() . '/assets/css/main.min.css', array(), filemtime( get_template_directory() . '/assets/css/main.min.css' ), false );
 
-		wp_enqueue_style( array( 'bootstrap', 'style' ) );
+		wp_enqueue_style( array( 'style' ) );
 	}
 }
 
